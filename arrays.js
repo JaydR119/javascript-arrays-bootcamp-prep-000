1c6d4879c6d22e40c692dcef268be817d381d44c
Jayd Robinson <jaydr119@gmail.com>
2019-08-21 19:12:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array = array.shift()
+  array.shift(element)
   return array
 }
 
