c484374f47e1ef551be84f130dd8afa563eb61fc
Jayd Robinson <jaydr119@gmail.com>
2019-08-21 18:26:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return newArray = [...array, element]
+  return newArray = [...array, element];
 };
